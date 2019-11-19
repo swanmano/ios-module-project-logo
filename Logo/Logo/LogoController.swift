d6b0b48a41ae22e4cab1c0d0cdb46cf29393e86a
Craig Swanson <cswanson02@me.com>
2019-11-18 23:23:53 -0600

    Created UIView for logo and the UIView controller file.

---
+++ Logo/Logo/LogoController.swift
@@ -0,0 +1,15 @@
+//
+//  LogoController.swift
+//  Logo
+//
+//  Created by Craig Swanson on 11/18/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+    
+}
---
+++ Logo/Logo/LogoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logo
+//
+//  Created by Craig Swanson on 11/18/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class LogoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
